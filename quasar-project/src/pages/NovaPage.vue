f8a6b4fe62eccacdf2eebfc696722972b8dbf4bf
frenkie1521 <diego.banovac10@gmail.com>
2025-01-15 09:14:55 +0100

    Promjene za ishot 1 i 2

---
+++ quasar-project/src/pages/NovaPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner inline-actions class="text-white bg-red">
+      You have lost connection to the internet. This app is offline.
+      <template v-slot:action>
+        <q-btn flat color="white" label="Turn ON Wifi" />
+      </template>
+    </q-banner>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
